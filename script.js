ad7ce6395c26dd882c4e6e0a867b6c3a5de0f7fc
Cleiton Marcelino <cleitonmarcelino18@gmail.com>
2021-04-01 22:05:14 -0300

    Adicionado mais operações

--- script.js
+++ script.js
@@ -28,3 +28,33 @@ function subtracao(){
     input1.value='';
     input2.value='';
 }
+function multiplicacao(){
+    var inputs = document.getElementsByTagName('input');
+    var input1 = inputs [0];
+    var input2 = inputs [1];
+    var valor1 = parseFloat(input1.value);
+    var valor2 = parseFloat(input2.value);
+
+    if (valor1 === 0 && valor2 === 0 ) {
+        alert('Um dos campos está vazio');
+        return;
+    }
+    window.alert("O Valor da multiplicacao é :" + (valor1 * valor2));
+    input1.value='';
+    input2.value='';
+}
+function divisao(){
+    var inputs = document.getElementsByTagName('input');
+    var input1 = inputs [0];
+    var input2 = inputs [1];
+    var valor1 = parseFloat(input1.value);
+    var valor2 = parseFloat(input2.value);
+
+    if (valor1 === 0 && valgor2 === 0 ) {
+        alert('Um dos campos está vazio');
+        return;
+    }
+    window.alert("O Valor da divisao é :" + (valor1 / valor2));
+    input1.value='';
+    input2.value='';
+}
